8c3c262c910efeba61521e117a52d7cb1212aaff
Andreja Grujić <36510898+AndySrb@users.noreply.github.com>
2020-02-18 19:36:12 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "SourceCodePro:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "FontAwesome:pixelsize=14:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
@@ -15,8 +15,7 @@ static int borderpx = 2;
  * 2: utmp option
  * 3: SHELL environment variable
  * 4: value of shell in /etc/passwd
- * 5: value of shell in config.h
- */
+ * 5: value of shell in config.h */
 static char *shell = "/bin/sh";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
@@ -84,31 +83,32 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.92;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#060101", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#B14E1A",
+	"#DA6215",
+	"#B37244",
+	"#F09133",
+	"#EDAA4B",
+	"#F6D067",
+	"#FCF6BF",
+	"#696666",
+	"#B14E1A",
+	"#DA6215",
+	"#B37244",
+	"#F09133",
+	"#EDAA4B",
+	"#F6D067",
+	"#FCF6BF",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
 	"#282828", /* 258 -> bg */
+	//"black", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
 };
 
